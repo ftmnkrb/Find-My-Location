ef9cdf8d36f4db8fccf836ff39927f4e34497166
ftmnkrb <fkarabacak026@gmail.com>
2023-09-03 19:00:37 +0300

    enlem, boylam, ülke,şehir,bölge

--- script.js
+++ script.js
@@ -5,10 +5,28 @@ document.querySelector("#btnLocation").addEventListener("click", () => {
 });
 
 function onError(err) {
-
+     console.error("Konum alınamadı: " + err.message);
 }
-function onSuccess (position) {
+async function onSuccess (position) {
      let lat = position.coords.latitude;
      let lng = position.coords.longitude;
      console.log(lat, lng)
+
+     // Konum bilgilerini ekrana yazdır
+    const locationInfo = document.getElementById("locationInfo");
+    locationInfo.innerHTML = `Enlem: ${lat}, Boylam: ${lng}`;
+
+    const api_key = "5a4281892cbd4cfeb53ce9900dc61710";
+    const url = `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lng}&key=${api_key}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+     const country = data.results[0].components.country; 
+     let locationInfoCountry = document.querySelector("#locationInfoCountry");
+     let locationInfoRegion = document.querySelector("#locationInfoRegion");
+     let locationInfoCity = document.querySelector("#locationInfoCity");
+     locationInfoCountry.innerHTML= `Bulunduğunuz Ülke : ${country}`
+     locationInfoRegion.innerHTML= `Bulunduğunuz Bölge : ${data.results[0].components.region}`
+     locationInfoCity.innerHTML= `Bulunduğunuz Şehir : ${data.results[0].components.city}`
 }
